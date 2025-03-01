5b3a8bb6f243117577b84fb83eb020cfbe454ef7
DevGuilhermeRodrigues <guilhermegrillo25@gmail.com>
2025-02-28 22:14:22 -0300

    menu responsivo

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const menu = document.querySelector('.menu-hamburguer')
 const iconeMenu = document.querySelector(".icone-menu")
+const menuHamburguer = document.querySelector('.navigation')
+const menuList = document.querySelector(".navigation-list")
 
 menu.addEventListener("click", function(){
     iconeMenu.src = (iconeMenu.src.includes("img/menu-line.svg")) ? "img/close-large-line.svg" : "img/menu-line.svg";
+    menuHamburguer.classList.toggle('active');
+    menuList.classList.toggle('active');
 })
 
